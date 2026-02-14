aec2df8941898463e9d2c24f1a6d2cde2551023c
mesl <mesarwarlemon@gmail.com>
2026-02-14 21:36:21 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,45 +1,22 @@
-const CACHE_NAME = "attendance-v2";
-
-const CACHED_FILES = [
-  "./",
-  "index.html",
-  "dashboard.html",
-  "admin.html",
-  "manifest.json",
-  "icon-192.png",
-  "icon-512.png"
+const CACHE_NAME = "ict-attendance-v1";
+const urlsToCache = [
+    "index.html",
+    "manifest.json",
+    "icon-192.png",
+    "icon-512.png",
+    "https://cdn.jsdelivr.net/npm/papaparse@5.4.1/papaparse.min.js"
 ];
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(CACHED_FILES);
-    })
-  );
-  self.skipWaiting();
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(cache => cache.addAll(urlsToCache))
+    );
 });
 
-self.addEventListener("activate", e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-      )
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        const resClone = res.clone();
-        caches.open(CACHE_NAME).then(cache => {
-          cache.put(e.request, resClone);
-        });
-        return res;
-      })
-      .catch(() => caches.match(e.request))
-  );
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request)
+        .then(response => response || fetch(event.request))
+    );
 });
